64eddaaf9759b87e94f2c284d10bbf3f8635de26
Artem <lukart80@yandex.ru>
2022-04-14 18:07:45 +0300

    done

--- pep_parse/pipelines.py
+++ pep_parse/pipelines.py
@@ -1,13 +1,32 @@
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+import datetime as dt
+from collections import Counter
+from pathlib import Path
 
-
-# useful for handling different item types with a single interface
-from itemadapter import ItemAdapter
+BASE_DIR = Path(__file__).parent.parent
+DATETIME_FORMAT = '%Y-%m-%d_%H-%M-%S'
 
 
 class PepParsePipeline:
+
+    def __init__(self):
+        self.counter = Counter()
+
+    def open_spider(self, spider):
+        pass
+
     def process_item(self, item, spider):
+        self.counter[item['status']] += 1
         return item
+
+    def close_spider(self, spider):
+        result_dir = BASE_DIR / 'results'
+        result_dir.mkdir(exist_ok=True)
+        now = dt.datetime.now()
+        now_formatted = now.strftime(DATETIME_FORMAT)
+        file_name = f'status_summary_{now_formatted}.csv'
+        file_dir = result_dir / file_name
+        with open(file_dir, mode='w', encoding='utf-8') as f:
+            f.write('Статус,Количество\n')
+            for status, count in self.counter.items():
+                f.write(f'{status},{count}\n')
+            f.write(f'Total,{sum(self.counter.values())}')
--- pep_parse/spiders/pep.py
+++ pep_parse/spiders/pep.py
@@ -1,10 +1,29 @@
 import scrapy
 
+from pep_parse.items import PepParseItem
+
 
 class PepSpider(scrapy.Spider):
     name = 'pep'
     allowed_domains = ['peps.python.org']
-    start_urls = ['http://peps.python.org/']
+    start_urls = ['https://peps.python.org/']
 
     def parse(self, response):
-        pass
+        section = response.css('#index-by-category')
+        links = section.css('td.num a::attr(href)').getall()
+        for link in links:
+            yield response.follow(link, callback=self.parse_pep)
+
+    def parse_pep(self, response):
+        item = PepParseItem(
+            number=response.css('#pep-content dd::text').get().strip(''""''),
+            name=response.css('#pep-content dd.field-even::text').get(),
+            status=response
+            .xpath('//dt[contains(text(), "Status")]/following-sibling::dd['
+                   '1]/text()')
+            .get()
+
+
+
+        )
+        yield item
